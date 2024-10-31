3d607f88a4a334d97b94f2a6ca0ee9adbb0fff44
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-10-31 09:17:42 +0900

    feat: 카테고리 생성 인터페이스 메서드 생성

--- src/main/java/com/nhnacademy/bookstore/bookset/category/service/CategoryService.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/category/service/CategoryService.java
@@ -1,4 +1,10 @@
 package com.nhnacademy.bookstore.bookset.category.service;
 
+import com.nhnacademy.bookstore.bookset.category.dto.request.CreateCategoryRequest;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface CategoryService {
+
+    Long createCategory(CreateCategoryRequest request);
 }
