a6fc53b1090b6e144d18a58c89559dab897a8732
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-10-31 09:18:17 +0900

    feat: 카테고리 서비스 인터페이스의 createCategory() 구현

--- src/main/java/com/nhnacademy/bookstore/bookset/category/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/category/service/Impl/CategoryServiceImpl.java
@@ -1,4 +1,29 @@
 package com.nhnacademy.bookstore.bookset.category.service.Impl;
 
-public class CategoryServiceImpl {
+import com.nhnacademy.bookstore.bookset.category.dto.request.CreateCategoryRequest;
+import com.nhnacademy.bookstore.bookset.category.entity.Category;
+import com.nhnacademy.bookstore.bookset.category.repository.CategoryRepository;
+import com.nhnacademy.bookstore.bookset.category.service.CategoryService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Long createCategory(CreateCategoryRequest request) {
+        Category subcategory = request.subcategoryId() != null ?
+                categoryRepository.findByCategoryId(request.subcategoryId().getCategoryId()).orElse(null) : null;
+
+        Category category = Category.builder()
+                .subcategory(subcategory)
+                .name(request.categoryName())
+                .build();
+        return categoryRepository.save(category).getCategoryId();
+    }
 }
