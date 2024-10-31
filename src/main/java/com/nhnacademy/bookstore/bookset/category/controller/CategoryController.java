f10c0c8fafea662271240435ef962559d09cccd4
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-10-31 09:49:58 +0900

    refactor: 불필요한 import 제거

--- src/main/java/com/nhnacademy/bookstore/bookset/category/controller/CategoryController.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/category/controller/CategoryController.java
@@ -4,7 +4,6 @@ import com.nhnacademy.bookstore.bookset.category.dto.request.CreateCategoryReque
 import com.nhnacademy.bookstore.bookset.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
