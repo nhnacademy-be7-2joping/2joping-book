6a33118b47f6de19be74de0bc2ab8bd02152d92c
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-10-31 09:11:49 +0900

    feat: 카테고리 생성 포스트 매핑 구현

--- src/main/java/com/nhnacademy/bookstore/bookset/category/controller/CategoryController.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/category/controller/CategoryController.java
@@ -1,4 +1,25 @@
 package com.nhnacademy.bookstore.bookset.category.controller;
 
+import com.nhnacademy.bookstore.bookset.category.dto.request.CreateCategoryRequest;
+import com.nhnacademy.bookstore.bookset.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("/categories")
+    public ResponseEntity<Long> createCategory(@RequestBody CreateCategoryRequest request) {
+        Long categoryId = categoryService.createCategory(request);
+        return ResponseEntity.created(URI.create("/" + categoryId)).build();
+    }
 }
