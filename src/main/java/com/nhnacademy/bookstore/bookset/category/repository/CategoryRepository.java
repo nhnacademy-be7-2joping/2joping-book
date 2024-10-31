2cd9c0ecbc3da86bad9d2416805a19ccf729335a
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-10-31 09:17:07 +0900

    feat: 요청 바디의 카테고리 아이디로 DB의 카테고리 찾는 find 함수 생성

--- src/main/java/com/nhnacademy/bookstore/bookset/category/repository/CategoryRepository.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/category/repository/CategoryRepository.java
@@ -3,6 +3,9 @@ package com.nhnacademy.bookstore.bookset.category.repository;
 import com.nhnacademy.bookstore.bookset.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    Optional<Category> findByCategoryId(Long categoryId);
 }
