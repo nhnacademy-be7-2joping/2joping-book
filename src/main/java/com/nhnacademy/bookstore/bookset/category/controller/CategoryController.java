0dfae1d46c419c2dd949ce9d994d1d4c57b149ee
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-11-04 14:11:53 +0900

    feat: update, delete API 메서드 작성

--- src/main/java/com/nhnacademy/bookstore/bookset/category/controller/CategoryController.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/category/controller/CategoryController.java
@@ -1,14 +1,18 @@
 package com.nhnacademy.bookstore.bookset.category.controller;
 
 import com.nhnacademy.bookstore.bookset.category.dto.request.CreateCategoryRequest;
+import com.nhnacademy.bookstore.bookset.category.dto.request.UpdateCategoryRequest;
+import com.nhnacademy.bookstore.bookset.category.dto.response.GetAllCategoryResponse;
 import com.nhnacademy.bookstore.bookset.category.dto.response.GetCategoryResponse;
+import com.nhnacademy.bookstore.bookset.category.dto.response.UpdateCategoryResponse;
 import com.nhnacademy.bookstore.bookset.category.service.CategoryService;
-import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,10 +27,27 @@ public class CategoryController {
     }
 
     @GetMapping("/categories/{categoryId}")
-    public ResponseEntity<GetCategoryResponse> getCategory(@Valid @PathVariable Long categoryId) {
-        GetCategoryResponse response =categoryService.getCategory(categoryId);
+    public ResponseEntity<GetCategoryResponse> getCategory(@Positive @PathVariable Long categoryId) {
+        GetCategoryResponse response = categoryService.getCategory(categoryId);
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/categories")
+    public ResponseEntity<List<GetAllCategoryResponse>> getAllCategories() {
+        List<GetAllCategoryResponse> response = categoryService.getAllCategories();
+        return ResponseEntity.ok(response);
+    }
 
+    @PutMapping("/categories/{categoryId}")
+    public ResponseEntity<UpdateCategoryResponse> updateCategory(@Positive @PathVariable Long categoryId,
+                                           @RequestBody UpdateCategoryRequest request) {
+        UpdateCategoryResponse response = categoryService.updateCategory(categoryId, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/categories/{categoryId}")
+    public ResponseEntity<Long> deleteCategory(@PathVariable Long categoryId) {
+        Long deletedCategoryId = categoryService.deleteCategory(categoryId);
+        return ResponseEntity.ok(deletedCategoryId);
+    }
 }
