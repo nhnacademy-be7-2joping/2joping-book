80b407779e8584ffbaac60de2ea4ba8971a89a30
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-11-07 15:08:43 +0900

    refactor: subcategory -> parentCategory 필드명 수정으로 인한 메서드 명 수정

--- src/main/java/com/nhnacademy/bookstore/bookset/category/service/CategoryService.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/category/service/CategoryService.java
@@ -1,8 +1,8 @@
 package com.nhnacademy.bookstore.bookset.category.service;
 
-import com.nhnacademy.bookstore.bookset.category.dto.request.CreateCategoryRequest;
+import com.nhnacademy.bookstore.bookset.category.dto.request.CategoryCreateRequest;
 import com.nhnacademy.bookstore.bookset.category.dto.request.UpdateCategoryRequest;
-import com.nhnacademy.bookstore.bookset.category.dto.response.GetAllCategoryResponse;
+import com.nhnacademy.bookstore.bookset.category.dto.response.GetAllCategoriesResponse;
 import com.nhnacademy.bookstore.bookset.category.dto.response.GetCategoryResponse;
 import com.nhnacademy.bookstore.bookset.category.dto.response.UpdateCategoryResponse;
 import org.springframework.stereotype.Service;
@@ -12,9 +12,9 @@ import java.util.List;
 @Service
 public interface CategoryService {
 
-    Long createCategory(CreateCategoryRequest request);
+    Long createCategory(CategoryCreateRequest request);
     GetCategoryResponse getCategory(Long categoryId);
-    List<GetAllCategoryResponse> getAllCategories();
+    List<GetAllCategoriesResponse> getAllCategories();
     UpdateCategoryResponse updateCategory(Long categoryId, UpdateCategoryRequest request);
     Long deleteCategory(Long categoryId);
 }
