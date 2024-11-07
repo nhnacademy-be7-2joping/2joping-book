01697b57b84eca0824105999031684b13f1a94f4
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-11-07 15:08:17 +0900

    refactor: subcategory -> parentCategory 필드명 수정으로 인한 메서드 명 수정

--- src/main/java/com/nhnacademy/bookstore/bookset/category/repository/CategoryRepository.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/category/repository/CategoryRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Optional<Category> findByCategoryId(Long categoryId);
-    Optional<Category> findBySubcategory(Category subcategory);
+    Optional<Category> findByParentCategory(Category parentCategory);
     List<Category> findAllByOrderByNameAsc();
 }
