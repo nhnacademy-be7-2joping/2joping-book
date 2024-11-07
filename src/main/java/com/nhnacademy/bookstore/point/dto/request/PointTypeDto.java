60b5c42302f6b55a345d75d26f3d70797b65de22
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-11-07 13:59:48 +0900

    feat: 포인트 종류 DTO 생성

---
+++ src/main/java/com/nhnacademy/bookstore/point/dto/request/PointTypeDto.java
@@ -0,0 +1,23 @@
+package com.nhnacademy.bookstore.point.dto.request;
+
+import com.nhnacademy.bookstore.point.entity.PointType;
+import com.nhnacademy.bookstore.point.enums.PointTypeEnum;
+
+public record PointTypeDto(
+
+        Long id,
+        PointTypeEnum type,
+        Integer accVal,
+        String name,
+        boolean isActive
+) {
+    public static PointTypeDto from(PointType entity) {
+        return new PointTypeDto(
+                entity.getId(),
+                entity.getType(),
+                entity.getAccVal(),
+                entity.getName(),
+                entity.isActive()
+        );
+    }
+}
