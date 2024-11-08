3b9739aab54094eb8bfda84011dafb50b1c67d1f
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-11-08 14:58:59 +0900

    chore: 체크아웃을 위한 코드 삭제 ..

--- src/main/java/com/nhnacademy/bookstore/bookset/category/service/CategoryService.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/category/service/CategoryService.java
@@ -1,20 +1,7 @@
 package com.nhnacademy.bookstore.bookset.category.service;
 
-import com.nhnacademy.bookstore.bookset.category.dto.request.CreateCategoryRequest;
-import com.nhnacademy.bookstore.bookset.category.dto.request.UpdateCategoryRequest;
-import com.nhnacademy.bookstore.bookset.category.dto.response.GetAllCategoryResponse;
-import com.nhnacademy.bookstore.bookset.category.dto.response.GetCategoryResponse;
-import com.nhnacademy.bookstore.bookset.category.dto.response.UpdateCategoryResponse;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public interface CategoryService {
-
-    Long createCategory(CreateCategoryRequest request);
-    GetCategoryResponse getCategory(Long categoryId);
-    List<GetAllCategoryResponse> getAllCategories();
-    UpdateCategoryResponse updateCategory(Long categoryId, UpdateCategoryRequest request);
-    Long deleteCategory(Long categoryId);
 }
