02aded2f622f87780768ff2c316b6687dd7fea6e
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-11-12 14:34:52 +0900

    feat: 도서 주문 및 리뷰 작성 시 포인트 추가 기능 구현 조금 더 구현할 것

--- src/main/java/com/nhnacademy/bookstore/point/service/impl/PointServiceImpl.java
+++ src/main/java/com/nhnacademy/bookstore/point/service/impl/PointServiceImpl.java
@@ -2,6 +2,7 @@ package com.nhnacademy.bookstore.point.service.impl;
 
 import com.nhnacademy.bookstore.common.error.exception.point.SignUpPointPolicyNotFoundException;
 import com.nhnacademy.bookstore.common.error.exception.user.member.MemberNotFoundException;
+import com.nhnacademy.bookstore.orderset.order.entity.Order;
 import com.nhnacademy.bookstore.point.dto.request.PointHistoryDto;
 import com.nhnacademy.bookstore.point.dto.request.PointUseRequest;
 import com.nhnacademy.bookstore.point.entity.PointHistory;
@@ -24,23 +25,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PointServiceImpl implements PointService {
 
+    private static final int REVIEW_POINT = 500;
+
     private final PointTypeRepository pointTypeRepository;
     private final PointHistoryRepository pointHistoryRepository;
     private final MemberRepository memberRepository;
 
     @Transactional
-    public void awardSignUpPoint(Long customerId) {
-        PointType signUpPointType = pointTypeRepository.findByNameAndIsActiveTrue("회원가입")
-                .orElseThrow(() -> new SignUpPointPolicyNotFoundException("회원가입 포인트 정책을 찾을 수 없습니다."));
-
-        Member member = memberRepository.findById(customerId)
-                .orElseThrow(() -> new MemberNotFoundException("회원을 찾을 수 없습니다."));
-
-        Integer pointAmount = signUpPointType.getAccVal();
-        // TODO: 회원가입 시 해당 멤버의 포인트 +5000 로직 추가
-    }
-
-    @Transactional
+    @Override
     public void awardReviewPoint(Long customerId, Long orderDetailId) {
         PointType reviewPointType = pointTypeRepository.findByNameAndIsActiveTrue("리뷰작성")
                 .orElseThrow(() -> new EntityNotFoundException("리뷰 포인트 정책을 찾을 수 없습니다."));
@@ -49,14 +41,37 @@ public class PointServiceImpl implements PointService {
                 .orElseThrow(() -> new EntityNotFoundException("회원을 찾을 수 없습니다."));
 
         Integer pointAmount = reviewPointType.getAccVal();
+
         // TODO: 리뷰 작성 시 해당 멤버의 포인트 +500 로직 추가
+        member.addPoint(REVIEW_POINT);
 
         createPointHistory(reviewPointType, orderDetailId, null, customerId, pointAmount);
     }
 
-    // TODO: 구매 시 적립 포인트 관련 메서드
+    // TODO: 결제 시 적립 포인트 관련 메서드
+    // 멤버가 주문 완료했을 때
+    // 어떤 주문인지 주문 정보를 찾고
+    // 어떤 멤버인지 멤버 정보를 찾고
+    // 해당 주문에 대해서 구매 적립 포인트 추가
+    // 추가 완료 return
+    @Transactional
+    @Override
+    public void awardOrderPoint(Long customerId, Long orderId) {
+        Member member = memberRepository.findById(customerId)
+                .orElseThrow(() -> new MemberNotFoundException("회원을 찾을 수 없습니다."));
+
+        PointType reviewPointType = pointTypeRepository.findByNameAndIsActiveTrue("도서 결제")
+                .orElseThrow(() -> new EntityNotFoundException("리뷰 포인트 정책을 찾을 수 없습니다."));
+
+//        Order order = orderRepository.findByOrderId(orderId);
+//        int totalPrice = order.getTotalPrice();
+//
+//        int amount = (int) Math.floor(totalPrice);
+//        member.addPoint(amount);
+    }
 
     @Transactional
+    @Override
     public void usePoint(PointUseRequest request) {
         Member member = memberRepository.findById(request.customerId())
                 .orElseThrow(() -> new MemberNotFoundException("회원을 찾을 수 없습니다."));
@@ -71,6 +86,7 @@ public class PointServiceImpl implements PointService {
                 request.customerId(), -request.pointAmount());
     }
 
+    @Override
     public List<PointHistoryDto> getPointHistory(Long customerId) {
         return pointHistoryRepository.findByCustomerId(customerId)
                 .stream()
