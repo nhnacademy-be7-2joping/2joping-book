981b3f57d2ddccaa21484be45add3f6bbe22ec95
choeunlee <choeun1500@gmail.com>
2024-11-19 19:51:01 +0900

    feat: 도서 단독 등록 구현

--- src/main/java/com/nhnacademy/bookstore/bookset/book/service/BookService.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/book/service/BookService.java
@@ -1,15 +1,29 @@
 package com.nhnacademy.bookstore.bookset.book.service;
 
+import com.nhnacademy.bookstore.bookset.book.dto.request.BookCreateRequestDto;
+import com.nhnacademy.bookstore.bookset.book.dto.response.BookCreateResponseDto;
 import com.nhnacademy.bookstore.bookset.book.dto.response.BookResponseDto;
 import com.nhnacademy.bookstore.bookset.book.dto.response.BookSimpleResponseDto;
+import com.nhnacademy.bookstore.bookset.book.entity.Book;
+import com.nhnacademy.bookstore.bookset.category.entity.Category;
+import com.nhnacademy.bookstore.bookset.contributor.dto.response.ContributorResponseDto;
+import com.nhnacademy.bookstore.bookset.tag.dto.TagResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 
 
 public interface BookService {
+    BookCreateResponseDto createBook(BookCreateRequestDto bookCreateRequestDto);
     Page<BookSimpleResponseDto> getAllBooks(Pageable pageable);
     BookResponseDto getBookById(Long bookId);
     Page<BookSimpleResponseDto> getBooksByCategoryId(Pageable pageable, Long categoryId);
     Page<BookSimpleResponseDto> getBooksByContributorId(Pageable pageable, Long contributorId);
+    List<ContributorResponseDto> getContributorList(String text);
+    Category getLowestLevelCategory(String categoryText);
+    List<TagResponseDto> associateBookWithTag(Book book, String text);
+
 }
