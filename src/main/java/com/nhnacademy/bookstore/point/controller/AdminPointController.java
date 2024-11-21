5b64e61d0a9bf6db62c9d96d6338dcd1689b660b
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-11-21 13:43:25 +0900

    fix: 의존성 주입 어노테이션 및 final 키워드 추가

--- src/main/java/com/nhnacademy/bookstore/point/controller/AdminPointController.java
+++ src/main/java/com/nhnacademy/bookstore/point/controller/AdminPointController.java
@@ -3,14 +3,16 @@ package com.nhnacademy.bookstore.point.controller;
 import com.nhnacademy.bookstore.point.dto.response.PointTypeDto;
 import com.nhnacademy.bookstore.point.service.impl.PointTypeServiceImpl;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/points")
+@RequiredArgsConstructor
 public class AdminPointController {
 
-    private PointTypeServiceImpl pointTypeServiceImpl;
+    private final PointTypeServiceImpl pointTypeServiceImpl;
 
     @PostMapping("/types")
     public ResponseEntity<PointTypeDto> createPointType(@RequestBody @Valid PointTypeDto request) {
@@ -19,7 +21,7 @@ public class AdminPointController {
 
     @PutMapping("/types/{id}")
     public ResponseEntity<PointTypeDto> updatePointType(
-            @PathVariable Long id,
+            @PathVariable("id") Long id,
             @RequestBody @Valid PointTypeDto request
     ) {
         return ResponseEntity.ok(pointTypeServiceImpl.updatePointType(id, request));
