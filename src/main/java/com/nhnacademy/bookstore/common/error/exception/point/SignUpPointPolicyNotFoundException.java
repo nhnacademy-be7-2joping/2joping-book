77e7211595ad3205ea2a9ea44a7a671984a8f847
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-11-21 15:59:04 +0900

    fix: 없는 클래스 복붙

---
+++ src/main/java/com/nhnacademy/bookstore/common/error/exception/point/SignUpPointPolicyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.bookstore.common.error.exception.point;
+
+public class SignUpPointPolicyNotFoundException extends RuntimeException {
+    public SignUpPointPolicyNotFoundException(String message) {
+        super(message);
+    }
+}
