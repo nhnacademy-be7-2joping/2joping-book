ddc8e59381879eafbbf37a42d86f7464208e60f4
chaeyeon <yeonicy@naver.com>
2024-11-25 23:05:33 +0900

    hotfix: 중복 메서드 삭제, 에러 test code주석처리

--- src/main/java/com/nhnacademy/bookstore/bookset/book/service/BookService.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/book/service/BookService.java
@@ -25,11 +25,9 @@ public interface BookService {
     Page<BookSimpleResponseDto> getBooksByCategoryId(Pageable pageable, Long categoryId);
     Page<BookSimpleResponseDto> getBooksByContributorId(Pageable pageable, Long contributorId);
     // List<ContributorResponseDto> getContributorList(String text);
-    Category getLowestLevelCategory(String categoryText);
-    List<TagResponseDto> associateBookWithTag(Book book, String text);
+    //    Category getLowestLevelCategory(String categoryText);
     BookUpdateResponseDto getUpdateBookByBookId(Long bookId);
     BookUpdateResultResponseDto updateBook(Long bookId, BookUpdateRequestDto bookUpdateRequestDto);
-    List<ContributorResponseDto> getContributorList(String contributorListJson);
     List<ContributorResponseDto> getContributorListForAPI(String text);
     Category getLowestLevelCategory(String categoryText);
     List<ContributorResponseDto> getContributorList(String contributorListJson);
--- src/main/java/com/nhnacademy/bookstore/bookset/publisher/service/PublisherServiceImpl.java
+++ src/main/java/com/nhnacademy/bookstore/bookset/publisher/service/PublisherServiceImpl.java
@@ -95,22 +95,6 @@ public class PublisherServiceImpl implements PublisherService{
         return responseDtoList;
     }
 
-    /**
-     * 모든 출판사를 조회하는 메서드 (등록용)
-     * @return -> 출판사 객체
-     */
-    @Override
-    @Transactional(readOnly = true)
-    public List<PublisherResponseDto> getAllPublishersForRegister() {
-        List<Publisher> publishers = publisherRepository.findAll();
-        List<PublisherResponseDto> responseDtoList = new ArrayList<>();
-
-        for (Publisher publisher : publishers) {
-            PublisherResponseDto dto = new PublisherResponseDto(publisher.getPublisherId(), publisher.getName());
-            responseDtoList.add(dto);
-        }
-        return responseDtoList;
-    }
 
     /**
      * 특정 출판사를 삭제하는 메서드
