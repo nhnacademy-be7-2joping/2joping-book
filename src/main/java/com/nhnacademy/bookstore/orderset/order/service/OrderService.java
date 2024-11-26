a60e32be29ebf80a635bc179713cd5042a267073
Sauter001 <lsj457@naver.com>
2024-11-26 22:01:43 +0900

    feat: add wrap info

--- src/main/java/com/nhnacademy/bookstore/orderset/order/service/OrderService.java
+++ src/main/java/com/nhnacademy/bookstore/orderset/order/service/OrderService.java
@@ -1,7 +1,13 @@
 package com.nhnacademy.bookstore.orderset.order.service;
 
+import com.nhnacademy.bookstore.admin.wrap.entity.Wrap;
+import com.nhnacademy.bookstore.admin.wrap.entity.WrapManage;
+import com.nhnacademy.bookstore.admin.wrap.repository.WrapManageRepository;
+import com.nhnacademy.bookstore.admin.wrap.repository.WrapRepository;
+import com.nhnacademy.bookstore.admin.wrap.service.WrapService;
 import com.nhnacademy.bookstore.bookset.book.entity.Book;
 import com.nhnacademy.bookstore.bookset.book.repository.BookRepository;
+import com.nhnacademy.bookstore.common.error.exception.bookset.book.BookNotFoundException;
 import com.nhnacademy.bookstore.coupon.entity.member.MemberCoupon;
 import com.nhnacademy.bookstore.coupon.repository.member.MemberCouponRepository;
 import com.nhnacademy.bookstore.orderset.order.dto.request.OrderPostRequest;
@@ -19,7 +25,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -29,10 +38,13 @@ public class OrderService {
 
     private final RedisTemplate<Object, Object> redisTemplate;
     private final OrderStateService orderStateService;
+    private final WrapService wrapService;
     private final BookRepository bookRepository;
     private final MemberCouponRepository memberCouponRepository;
     private final OrderRepository orderRepository;
     private final OrderDetailRepository orderDetailRepository;
+    private final WrapRepository wrapRepository;
+    private final WrapManageRepository wrapManageRepository;
 
     public void registerOrderOnRedis(OrderRequest orderRequest) {
         // 주문 임시 저장 10분 동안 유효
@@ -59,11 +71,22 @@ public class OrderService {
 
         // 주문 상세 등록
         List<OrderRequest.CartItemRequest> cartItemRequests = orderRequest.cartItemList();
+        Map<Long, OrderDetail> orderDetailMap = new LinkedHashMap<>();
         for (OrderRequest.CartItemRequest cartItemRequest : cartItemRequests) {
-            Book book = bookRepository.findById(cartItemRequest.bookId()).orElse(null);
+            Book book = bookRepository.findById(cartItemRequest.bookId()).orElseThrow(BookNotFoundException::new);
             OrderDetail orderDetail = new OrderDetail();
             orderDetail.apply(savedOrder, book, cartItemRequest.quantity(), cartItemRequest.unitPrice());
-            orderDetailRepository.save(orderDetail);
+            orderDetailMap.put(book.getBookId(), orderDetailRepository.save(orderDetail));
+        }
+
+        // 주문 포장 등록
+        List<OrderRequest.WrapItemRequest> validWrapRequests =
+                orderRequest.wrapList().stream().filter(w -> Objects.nonNull(w.wrapId())).toList();
+        for (OrderRequest.WrapItemRequest wrapItem : validWrapRequests) {
+            OrderDetail orderDetail = orderDetailMap.get(wrapItem.bookId());
+            Wrap wrap = wrapRepository.findById(wrapItem.wrapId()).orElse(null);
+            WrapManage wrapManage = new WrapManage(null, wrap, orderDetail);
+            wrapManageRepository.save(wrapManage);
         }
     }
 }
