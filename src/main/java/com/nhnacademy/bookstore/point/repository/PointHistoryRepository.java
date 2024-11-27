37d7623dd245dd6cab66c62368249dffc3c7b3c5
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-11-27 15:58:30 +0900

    feat: 레포지토리 페이징 처리

--- src/main/java/com/nhnacademy/bookstore/point/repository/PointHistoryRepository.java
+++ src/main/java/com/nhnacademy/bookstore/point/repository/PointHistoryRepository.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.bookstore.point.repository;
 
 import com.nhnacademy.bookstore.point.entity.PointHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,5 +18,6 @@ public interface PointHistoryRepository extends JpaRepository<PointHistory, Long
     @Query("select sum(p.pointVal) from PointHistory p where p.customerId =: customerId")
     Integer getTotalPointByCustomerId(@Param("customerId") Long customerId);
 
+    Page<PointHistory> findByCustomerIdOrderByRegisterDateDesc(Long customerId, Pageable pageable);
     List<PointHistory> findByCustomerIdOrderByRegisterDateDesc(Long customerId);
 }
