baefd57dcbde8e212cc96eb90882dd7ebc51e57a
yuhyun <dlugus@naver.com>
2024-11-29 10:33:12 +0900

    fix: 리뷰 예외처리 관련 버그 픽스

--- src/main/java/com/nhnacademy/bookstore/review/repository/ReviewRepository.java
+++ src/main/java/com/nhnacademy/bookstore/review/repository/ReviewRepository.java
@@ -4,5 +4,6 @@ import com.nhnacademy.bookstore.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
+    boolean existsByOrderDetail_OrderDetailId(Long orderDetailId);
 
 }
